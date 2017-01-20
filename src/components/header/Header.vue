c33bd6771a761f699d9ecb3fe56f064a4daabf12
srbtj <srbtj@aliyun.com>
2017-01-20 18:49:52 +0800

    header info

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="header">
+  <div class="header clear">
     <div class="content-wrap">
       <div class="avatar">
-        <img width="64" height="64" :src="seller.avatar" />
+        <img :src="seller.avatar"/>
       </div>
       <div class="content">
         <div class="title">
@@ -12,17 +12,63 @@
         <div class="desc">
           {{ seller.description }}/{{ seller.deliveryTime }}分钟送达
         </div>
-        <div v-if="seller.supports" class="discount">
-          <span class="icon"></span>
+        <div v-if="seller.supports" class="support">
+          <span class="icon" :class="clazzMap[seller.supports[0].type]"></span>
           <span class="text">{{ seller.supports[0].description }}</span>
+        </div>
+        <div class="person" @click="showMask">
+          <span>5个</span>
+          <i class="icon-keyboard_arrow_right"></i>
         </div>
       </div>
     </div>
-    <div class="ad-wrap"></div>
+    <div class="ad-wrap" @click="showMask">
+      <span class="icon"></span>
+      <span class="desc">{{ seller.bulletin }}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="bg-img">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <!-- 显示详情图层 -->
+    <transition name="maskFade">
+      <div class="mask"  v-show="hasShowMask">
+        <div class="mask-wrap">
+          <div class="mask-content">
+            <div class="title">{{ seller.name }}</div>
+            <div class="price">星条</div>
+            <div class="supports">
+              <div class="support-title">
+                <span class="line"></span>
+                <span class="text">优惠信息</span>
+                <span class="line"></span>
+              </div>
+              <ul class="supportList" v-if="seller.supports">
+                <li v-for="support in seller.supports">
+                  <span class="icon" :class="clazzMap[support.type]"></span>
+                  <span class="info">{{ support.description }}</span>
+                </li>
+              </ul>
+            </div>
+            <div class="sellerAd">
+              <div class="sellerAd-title">
+                <span class="line"></span>
+                <span class="text">商家公告</span>
+                <span class="line"></span>
+              </div>
+              <div class="sellerAd-desc">{{ seller.bulletin }}</div>
+            </div>
+          </div>
+        </div>
+        <div class="mask-close">
+          <i class="icon-close" @click="maskClose"></i>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 <style lang="less" type="text/less">
-  
+  @import "../../common/styles/header.less";
 </style>
 <script type="text/ecmascript-6">
   export default {
@@ -30,6 +76,22 @@
     props: {
       seller: {
         type: Object
+      }
+    },
+    data(){
+      return {
+        hasShowMask: false
+      };
+    },
+    created() {
+      this.clazzMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    methods: {
+      showMask() {
+        this.hasShowMask = true;
+      },
+      maskClose() {
+        this.hasShowMask = false;
       }
     }
   };
